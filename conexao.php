99b6a078195da09bf889a4936d48b06b6a4a7288
Yan Carlos <yancarlos55@yahoo.com.br>
2022-05-17 08:52:01 -0300

    construindo delete

--- conexao.php
+++ conexao.php
@@ -1,18 +1,20 @@
 <?php
 // Data Source Name
 // driver; endereço; nome do banco de dados; subconjunto de caracteres
+function testarConexao(){
 $DSN = "mysql:host=fdb25.awardspace.net;dbname=3451184_crud;charset=utf8";
 $usuario = "3451184_crud";
 $senha = "Crud%System08";
 // Tratamento de erro
 try {
     // Classe PFO de conexão
-    $conexao = new PDO($DSN, $usuario, $senha);
+   return $conexao = new PDO($DSN, $usuario, $senha);
   
 } catch (PDOException $erro) {
-    echo $erro->getMessage();
+   return echo $erro->getMessage();
 
     exit;
+}
 }
 
 ?>
