1f1492ccd87a8a6be3a3dfa1bf2c4d80596b4100
Yan Carlos <yancarlos55@yahoo.com.br>
2022-05-31 07:36:02 -0300

    Consertando delete

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ function autoInsertBD(list){
     btn.setAttribute("value", id);
     btn.setAttribute("class", "deleteButton");
     btn.setAttribute("type", "button");
-    btn.setAttribute("onclick", `editModal(${n})`);
+    btn.setAttribute("onclick", `editModal(${id})`);
     btn.setAttribute("data-bs-toggle", "modal");
     btn.setAttribute("data-bs-target", "#exampleModal");
     btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-x-lg" viewBox="0 0 16 16">
@@ -192,7 +192,7 @@ listAlunoBD().then((resultado) => autoInsertBD(resultado));
 
 var alunoSelecionado = null;
 
-function editModal(value) {
+const editModal = (value) => {
   alunoSelecionado = value;
   document.getElementById('sureDelete').innerText = "Deseja realmente Excluir o aluno (a) "+ value;
 }
